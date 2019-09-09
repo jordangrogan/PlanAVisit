010b3b9d6c7368c1c6238db00309e3b901f2b1fe
Jordan Grogan <jordangrogan11@gmail.com>
2019-09-09 00:16:32 -0400

    Refactored to use Promises for API calls

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const request = require('request')
-const bodyParser = require('body-parser');
+const request = require('request-promise')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -13,140 +13,150 @@ if (process.env.NODE_ENV !== 'production') {
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    console.log(process.env.PCO_APP_ID);
-    // request.get('https://api.planningcenteronline.com/people/v2/people/4960326', (error, response, body) => {
-    //     let person = JSON.parse(body)["data"]
-    //     res.send("Person name: " + person["attributes"]["name"])
-    //     console.error('error:', error) // Print the error if one occurred
-    //     console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-    //     console.log('body:', body) // Print the body
-    // }).auth(process.env.PCO_APP_ID, process.env.PCO_SECRET, false)
-    const postRequest = {
+    res.send("Plan a Visit at XR.church!")
+})
+
+app.post('/', (req, res) => {
+    const data = req.body
+
+    const campusID = (data.campus === "North Fayette") ? "9929" :
+        (data.campus === "Boyce Road") ? "9930" :
+        (data.campus === "Cranberry") ? "9931" :
+        (data.campus === "East Liberty") ? "9932" :
+        (data.campus === "Weirton") ? "9933" :
+        "9929"
+
+    // household is an array of Promises - each promise being a person being created
+    let household = []
+
+    // Check that we have data for first person
+    if (data.your_firstname && data.your_lastname) {
+
+        // Add person to Planning Center
+        const postPerson = request.post({
             uri: 'https://api.planningcenteronline.com/people/v2/people/',
             method: 'POST',
             json: {
                 "data": {
                     "attributes": {
-                        "first_name": "JordanTestPerson",
-                        "last_name": "JordanTestPersonLast"
+                        "first_name": data.your_firstname,
+                        "last_name": data.your_lastname,
+                        "primary_campus_id": campusID
                     }
                 }
             }
+        }).auth(process.env.PCO_APP_ID, process.env.PCO_SECRET, false).catch(error => { console.log(error.message) })
+
+        household.push(postPerson) // Add the Promise to the households array
+
+        // Add their email to their profile, if provided
+        if (data.your_email) {
+            const postEmail = postPerson.then(body => {
+                const personID = body["data"]["id"]
+                console.log("Person ID: " + personID)
+                return request.post({
+                    uri: `https://api.planningcenteronline.com/people/v2/people/${personID}/emails`,
+                    method: 'POST',
+                    json: {
+                        "data": {
+                            "attributes": {
+                                "address": data.your_email,
+                                "location": "Home",
+                                "primary": true
+                            }
+                        }
+                    }
+                }).auth(process.env.PCO_APP_ID, process.env.PCO_SECRET, false).catch(error => { console.log(error.message) })
+            })
         }
-        // request.post(postRequest, (error, response, body) => {
-        //     res.send("Posted.")
-        //     console.error('error:', error) // Print the error if one occurred
-        //     console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-        //     console.log('body:', body) // Print the body
-        // }).auth(process.env.PCO_APP_ID, process.env.PCO_SECRET, false)
-    res.send("Plan a visit!")
-})
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    const data = req.body
+        // Add their phone number to their profile, if provided
+        if (data.your_phone) {
+            const postPhone = postPerson.then(body => {
+                const personID = body["data"]["id"]
+                console.log("Person ID: " + body["data"]["id"])
+                return request.post({
+                    uri: `https://api.planningcenteronline.com/people/v2/people/${personID}/phone_numbers`,
+                    method: 'POST',
+                    json: {
+                        "data": {
+                            "attributes": {
+                                "number": data.your_phone,
+                                "location": "Mobile",
+                                "primary": true
+                            }
+                        }
+                    }
+                }).auth(process.env.PCO_APP_ID, process.env.PCO_SECRET, false).catch(error => { console.log(error.message) })
+            })
+        }
 
-    // Check that we have data for first person
-    if (data.your_firstname && data.your_lastname) {
-        // Add person to Planning Center
-        const you = addPersonToPlanningCenter(data.your_firstname, data.your_lastname, data.your_email, data.your_phone, data.campus)
     }
 
     // Check if we have spouse
     if (data.spouse_firstname && data.spouse_lastname) {
-        const spouse = addPersonToPlanningCenter(data.spouse_firstname, data.spouse_lastname, '', '', data.campus)
+
+        // Add spouse to Planning Center
+        const postSpouse = request.post({
+            uri: 'https://api.planningcenteronline.com/people/v2/people/',
+            method: 'POST',
+            json: {
+                "data": {
+                    "attributes": {
+                        "first_name": data.spouse_firstname,
+                        "last_name": data.spouse_lastname,
+                        "primary_campus_id": campusID
+                    }
+                }
+            }
+        }).auth(process.env.PCO_APP_ID, process.env.PCO_SECRET, false).catch(error => { console.log(error.message) })
+
+        household.push(postSpouse) // Add the Promise to the households array
     }
 
-    // TODO: householding
-
-
     // TODO: add children
+
+
+    // Put all the people in a household!
+    if (household.length > 0 && data.your_lastname) {
+
+        // Once all the people are created (every Promise in the household array has been resolved)
+        Promise.all(household).then(householdBodys => {
+
+            // Create the json to be POSTed
+            let householdJson = {
+                "data": {
+                    "attributes": {},
+                    "relationships": {
+                        "people": {
+                            "data": []
+                        },
+                        "primary_contact": {
+                            "data": {}
+                        }
+                    }
+                }
+            }
+            householdJson.data.attributes = { "name": data.your_lastname }
+            householdBodys.forEach((body) => {
+                householdJson.data.relationships.people.data.push({ "type": "Person", "id": `${body["data"]["id"]}` })
+            })
+            householdJson.data.relationships.primary_contact.data = { "type": "Person", "id": `${householdBodys[0]["data"]["id"]}` }
+            console.log(householdJson)
+
+            const postHousehold = request.post({
+                uri: 'https://api.planningcenteronline.com/people/v2/households/',
+                method: 'POST',
+                json: householdJson
+            }).auth(process.env.PCO_APP_ID, process.env.PCO_SECRET, false).catch(error => { console.log(error.message) })
+
+        })
+    }
+
 
     res.status(200)
     res.send("Success")
 })
 
-function addPersonToPlanningCenter(first, last, email, phone, campus) {
-
-    const campusID = (campus === "North Fayette") ? "9929" :
-        (campus === "Boyce Road") ? "9930" :
-        (campus === "Cranberry") ? "9931" :
-        (campus === "East Liberty") ? "9932" :
-        (campus === "Weirton") ? "9933" :
-        "9929"
-
-    let personID
-
-    const personPost = {
-        uri: 'https://api.planningcenteronline.com/people/v2/people/',
-        method: 'POST',
-        json: {
-            "data": {
-                "attributes": {
-                    "first_name": first,
-                    "last_name": last,
-                    "primary_campus_id": campusID
-                }
-            }
-        }
-    }
-
-    request.post(personPost, (error, response, body) => {
-        console.error('error:', error) // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-        console.log('body:', body) // Print the body
-        personID = body["data"]["id"]
-        console.log("Person ID: " + personID)
-        if (email) {
-            // Post the email address
-            const emailPost = {
-                uri: `https://api.planningcenteronline.com/people/v2/people/${personID}/emails`,
-                method: 'POST',
-                json: {
-                    "data": {
-                        "attributes": {
-                            "address": email,
-                            "location": "Home",
-                            "primary": true
-                        }
-                    }
-                }
-            }
-            request.post(emailPost, (error, response, body) => {
-                console.error('error:', error) // Print the error if one occurred
-                console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-                console.log('body:', body) // Print the body
-
-                if (phone) {
-                    // Post the phone number
-                    const phonePost = {
-                        uri: `https://api.planningcenteronline.com/people/v2/people/${personID}/phone_numbers`,
-                        method: 'POST',
-                        json: {
-                            "data": {
-                                "attributes": {
-                                    "number": phone,
-                                    "location": "Mobile",
-                                    "primary": true
-                                }
-                            }
-                        }
-                    }
-
-                    request.post(phonePost, (error, response, body) => {
-                        console.error('error:', error) // Print the error if one occurred
-                        console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-                        console.log('body:', body) // Print the body
-
-                    }).auth(process.env.PCO_APP_ID, process.env.PCO_SECRET, false)
-                }
-
-            }).auth(process.env.PCO_APP_ID, process.env.PCO_SECRET, false)
-        }
-
-    }).auth(process.env.PCO_APP_ID, process.env.PCO_SECRET, false)
-
-    return personID
-}
 
 app.listen(port, () => console.log(`PlanAVisit app listening on port ${port}!`))
